363ab2939040d17ef5e67752f9bdb974679da024
Pedro Guerrero <pedroguerrero@users.noreply.github.com>
2026-09-05 22:20:51 -0400

    add autopair

--- init.lua
+++ init.lua
@@ -1071,7 +1071,7 @@ do
 
   -- require 'kickstart.plugins.indent_line'
   -- require 'kickstart.plugins.lint'
-  -- require 'kickstart.plugins.autopairs'
+  require 'kickstart.plugins.autopairs'
   -- require 'kickstart.plugins.neo-tree'
   require 'kickstart.plugins.gitsigns' -- adds gitsigns recommended keymaps
 
